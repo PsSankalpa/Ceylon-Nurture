cd10ec1ca31e2a23107e25b5da7ac3d823be0754
AvishiJayaweera <86151177+AvishiJayaweera@users.noreply.github.com>
2021-10-13 23:40:56 -0700

    Made the view edit and delete in my account of the doctor

--- app/controllers/doctor.php
+++ app/controllers/doctor.php
@@ -4,11 +4,10 @@ class doctor extends Controller
     function index()
     {
       
-    //if(!Auth::logged_in())
-       //{
-         //  $this->redirect();
-
-      // }
+        if(!Auth::logged_in())
+        {
+         $this->redirect('login/login');
+        }
 
        $common_user = new common_user();
         $data = $common_user->findAll();
@@ -23,7 +22,7 @@ class doctor extends Controller
         if(count($_POST)>0)
         {
             
-            $doctors = new doctors();//create the instance of the seller in model
+            $doctors = new doctors();//create the instance of the doctor in model
             
             if($doctors->validate($_POST,$_FILES))
             {
@@ -34,7 +33,13 @@ class doctor extends Controller
                 $arr['dob'] = htmlspecialchars($_POST['dob']);
                 $arr['registrationNumber'] = htmlspecialchars($_POST['registrationNumber']);
                 $arr['specialities'] = htmlspecialchars($_POST['specialities']);
-                $arr['qualifications'] = $des;
+                $arr['hospital'] = htmlspecialchars($_POST['hospital']);
+                $arr['city'] = htmlspecialchars($_POST['city']);
+                $arr['address'] = htmlspecialchars($_POST['address']);
+                $arr['image'] = $des;
+
+                //print_r($arr);
+                //die;
              
                
                 $doctors->insert($arr);
@@ -48,7 +53,7 @@ class doctor extends Controller
 			'errors'=>$errors,
 		]);
     }
-     //get the file destination
+     //get the file destination of the image
     function get_destination($destination)
     {
          global $des;
@@ -56,15 +61,105 @@ class doctor extends Controller
          return $des;
     }
 
-    function account()
+    //function to view account
+    function viewAccount($userid=[])
     {
-      
-      // $account = new account();
+        $errors = array();
+        $doctors = new doctors();
         
-        $this->view("doctor/doctorAccount",$data); //in here put the relevent page name and the path
+
+        $row =$doctors->where('userid',$userid); //in here row is an array
+        $data2 = $doctors->findAll();
+        
+        $this->view('doctor/viewAccount',[
+			'errors'=>$errors,
+            'row'=>$row,
+            'data2'=>$data2,
+		]);
+
+    }
+    //function to edit account
+    function editAccount($userid = null)
+    {
+        if(!Auth::logged_in())
+        {
+         $this->redirect('login/login');
+        }
+
+        $errors = array();
+        $doctors = new doctors();
+        
+        if(count($_POST)>0)
+        {
+            
+            if($doctors->validate($_POST,$_FILES))
+            {
+                global $des;
+                $arr['userid'] = AUTH::userid();
+                $arr['nameWithInitials'] = $_POST['nameWithInitials'];
+                $arr['gender'] = $_POST['gender'];
+                $arr['dob'] = $_POST['dob'];
+                $arr['registrationNumber'] = $_POST['registrationNumber'];
+                $arr['specialities'] = $_POST['specialities'];
+                $arr['hospital'] = $_POST['hospital'];
+                $arr['city'] = $_POST['city'];
+                $arr['address'] = $_POST['address'];
+                $arr['image'] = $des;
+
+                $doctors->update($userid,$arr);
+                $this->redirect('doctor/viewAccount');
+            }
+            else{
+                $errors = $doctors->errors;
+            }
+        }
+        $row =$doctors->where('userid',$userid); 
+       // print_r($row);
+       //in here row is an array
+        
+        if($row)
+        {
+            $row = $row[0];
+            unlink($row->image);
+       
+        }
+        $this->view('doctor/editAccount',[
+			'errors'=>$errors,
+            'row'=>$row,
+		]);
+
+    }
+    //function to delete account
+    function deleteAccount($userid = null)
+    {
+        if(!Auth::logged_in())
+        {
+         $this->redirect('login/login');
+        }
+        $errors = array();
+        $doctors = new doctors();
+        
+        if(count($_POST)>0)
+        {
+            //print_r($data);
+            //die;
+                $doctors->delete($userid);
+                $this->redirect('doctor/viewAccount');
+            
+            
+        }
+        $row =$doctors->where('userid',$userid); //in here row is an array
+         if($row)
+        {
+            $row = $row[0];
+            unlink($row->image);
+        }
+        $this->view('doctor/deleteAccount',[
+            'row'=>$row,
+		]);
+
     }
 
-   
     //function to add schedule
     function addSchedule()
     {
@@ -72,6 +167,19 @@ class doctor extends Controller
         
         if(count($_POST)>0)
         {
+            $schedule = new schedule();
+            $doctor = new doctors();
+            $userid = Auth::userid();
+            if(!empty($row = $doctors->where('userid',$userid) ))
+            {
+               $row = $row[0];
+               $nameWithInitials= $row->nameWithInitials;
+               $city = $row->city;
+            }
+            else{
+                $nameWithInitials = "";
+                $city = "";
+            }
             //print_r($_POST);
   
             $schedule = new schedule();
@@ -86,6 +194,8 @@ class doctor extends Controller
                 $arr['timePerPatient'] = $_POST['timePerPatient'];
                 $arr['doctorCharge'] = $_POST['doctorCharge'];
                 $arr['doctorNote'] = $_POST['doctorNote'];
+                $arr['DoctorName'] = $nameWithInitials;
+                $arr['doctorid'] = Auth::userid();
               
                 //print_r($arr);
             
@@ -109,27 +219,6 @@ class doctor extends Controller
         $errors = array();
         $schedule = new schedule();
         
-        if(count($_POST)>0)
-        {
-            
-            if($schedule->validate($_POST,$_FILES))
-            {
-                $arr['slotNumber'] = $_POST['slotNumber'];
-                $arr['dateofSlot'] = $_POST['dateofSlot'];
-                $arr['arrivalTime'] = $_POST['arrivalTime'];
-                $arr['departureTime'] = $_POST['departureTime'];
-                $arr['noOfPatient'] = $_POST['noOfPatient'];
-                $arr['timePerPatient'] = $_POST['timePerPatient'];
-                $arr['doctorCharge'] = $_POST['doctorCharge'];
-                $arr['doctorNote'] = $_POST['doctorNote'];
-               
-                $schedule->update($scheduleid,$arr);
-                $this->redirect('doctor');
-            }
-            else{
-                $errors = $schedule->errors2;
-            }
-        }
         $row =$schedule->where('scheduleid',$scheduleid); //in here row is an array
         $data2 = $schedule->findAll();
         
@@ -140,6 +229,7 @@ class doctor extends Controller
 		]);
 
     }
+      //function to edit schedule
     function editSchedule($scheduleid = null)
     {
         if(!Auth::logged_in())
@@ -212,19 +302,14 @@ class doctor extends Controller
             //die;
                 $schedule->delete($scheduleid);
                 $this->redirect('doctor/viewSchedule');
-            
-            
         }
         $row =$schedule->where('scheduleid',$scheduleid); //in here row is an array
-       // $data2 =$schedule->where('scheduleid',$scheduleid);
          if($row)
         {
             $row = $row[0];
         }
         $this->view('doctor/deleteSchedule',[
             'row'=>$row,
-            //'rows'=>$data2,
-            //'data2'=>$data2,
 		]);
 
     }
