286b4856029e58f600eaac58878f301302197d27
PS1252 <piyumsankalpaps@gmail.com>
2021-10-17 21:10:37 +0530

    created the profile page and its functions,added the database

---
+++ app/views/profile/editCommonUser.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ceylon Nurture|Common|Edit</title>
+        <?php $this -> view ("header",$data)?>
+        <link rel="stylesheet" href="<?=ASSETS?>css/registrationform.css">
+
+    </head>
+
+    <body class="regi">
+
+    
+    <div class="container center">
+        <h1>Edit Common User Detials</h1>
+
+        <?php if($row):?>
+        <!--for the errors-->
+        <?php if(count($errors) > 0 ):?>
+        <div class="alertwarning">
+        <button class="closebtn" onclick="closebutton()">&times;</button>   
+            <strong>Error!</strong> 
+            <?php foreach($errors as $error ):?>
+                <br /><?=$error?>
+            <?php endforeach;?>
+        </div>
+        <?php endif;?>
+
+        <hr>
+        <form class="regi_form" enctype="multipart/form-data" method="POST">
+
+            <div class="row">
+            <div class="col-25">
+                <label for="fname">First Name</label>
+            </div>
+            <div class="col-75">
+                <input type="text" value="<?=get_var('fname',$row->fname)?>" id="fname" name="fname" placeholder="Name With Initials">
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="lname">Last Name</label>
+            </div>
+            <div class="col-75">
+                <input type="text" value="<?=get_var('lname',$row->lname)?>" id="lname" name="lname" placeholder="Registration Number">
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="email">Email</label>
+            </div>
+            <div class="col-75">
+            <input type="text" value="<?=get_var('email',$row->email)?>" id="email" name="email" placeholder="TP Number">
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="tpNumber">Tp Number</label>
+            </div>
+            <div class="col-75">
+            <input type="text" value="<?=get_var('tpNumber',$row->tpNumber)?>" id="tpNumber" name="tpNumber" placeholder="tpNumber">
+            </div>
+            </div>
+
+            <div class="row">
+            <input type="submit" value="Submit">
+            <input type="reset" value="Reset">
+            </div>
+        </form>
+        <?php endif;?>
+    </div>
+
+    <script type="text/javascript" src="<?=ASSETS?>js/sellerJs"></script>
+    
+    </body>
+</html>
---
+++ app/views/profile/editDoctor.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ceylon Nurture|Doctor|Edit</title>
+<?php $this -> view ("header",$data)?>
+<link rel="stylesheet" type="text/css" href="<?=ASSETS?>css/registrationform.css">
+</head>
+
+<body class="regi">
+
+    
+    <div class="container center">
+        <h1>Edit Doctor Details</h1>
+
+        <?php if($row):?>
+         <!--for the errors-->
+         <?php if(count($errors) > 0 ):?>
+        <div class="alertwarning">
+        <button class="closebtn" onclick="closebutton()">&times;</button>   
+            <strong>Error!</strong> 
+            <?php foreach($errors as $error ):?>
+                <br /><?=$error?>
+            <?php endforeach;?>
+        </div>
+        <?php endif;?>
+
+        <hr>
+        <form class="regi_form" enctype="multipart/form-data" method="POST">
+
+            <div class="row">
+            <div class="col-25">
+                <label for="nameWithInitials">Name With Initials</label>
+            </div>
+
+            <div class="col-75">
+                <input type="text" value="<?=get_var('nameWithInitials',$row->nameWithInitials)?>" id="nameWithInitials" name="nameWithInitials" placeholder="Name With Initials">
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="gender">Gender</label>
+            </div>
+
+            <div class="col-75">
+            <p class="gender">
+                <input type="radio" name="gender" value="male">Male
+                <input type="radio" name="gender" value="female">Female</p>
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="DOB">Date Of Birth</label>
+            </div>
+            <div class="col-75">
+            <input type="date" id="dob" name="dob" >
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="regNo">Registration Number</label>
+            </div>
+            <div class="col-75">
+            <input type="text" value="<?=get_var('registrationNumber',$row->registrationNumber)?>" id="registrationNumber" name="registrationNumber" placeholder="Fill your Medical Registration Number">
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="specialities">Specialities</label>
+            </div>
+            <div class="col-75">
+            <input type="text" value="<?=get_var('specialities',$row->specialities)?>" id="specialities" name="specialities" placeholder="Specialities">
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="Hospital">Hospital</label>
+            </div>
+            <div class="col-75">
+            <input type="text" value="<?=get_var('hospital',$row->hospital)?>" id="hospital" name="hospital" placeholder="Hospital">
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="City">City</label>
+            </div>
+            <div class="col-75">
+            <input type="text" value="<?=get_var('city',$row->city)?>" id="city" name="city" placeholder="City">
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="Address">Address</label>
+            </div>
+            <div class="col-75">
+            <input type="text" value="<?=get_var('address',$row->address)?>" id="address" name="address" placeholder="Address">
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="col-25">
+                <label for="image" >Qualifications</label>
+            </div>
+            <div class="col-75">
+            <input type="file" id="image" value="<?=get_var('image')?>" name="image">
+            </div>
+            </div>
+
+            <div class="row">
+            <input type="submit" value="Submit">
+            <input type="reset" value="Reset">
+            </div>
+        </form>
+        <?php endif;?>
+    </div>
+    </body>
+</html>
+
