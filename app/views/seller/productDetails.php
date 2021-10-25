caddb6e3b352e0e79de98bd94c3436abcb801427
PS1252 <piyumsankalpaps@gmail.com>
2021-10-25 19:24:41 +0530

    modified the seller and products

--- app/views/seller/productDetails.php
+++ app/views/seller/productDetails.php
@@ -1,41 +1,38 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Ceylon Nurture|Seller|Registration</title>
-        <?php $this -> view ("header",$data)?>
-        <link rel="stylesheet" href="<?=ASSETS?>css/sellers.css">
-    </head>
 
-    <body class="regi">
+<head>
+    <title>Ceylon Nurture|Seller|Registration</title>
+    <?php $this->view("header", $data) ?>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/sellers.css">
+</head>
 
-    
-    <div class="container center seller-grid-container">
-        <h2>Product</h2>
-        <h2>Product Details</h2>
-        
-        <?php foreach ($rows as $row):?>
-        <div>
-            <h3><?=$row->productName?> </h3>
-            <img src="<?=ASSETS2.$row->image?> ">
-        </div>
-        <div class="">
-            <h3>Details</h3>
+<body class="regi">
 
-            <div>
-                <h4>Price : <?="RS.".$row->productPrice?></h4>
-                <p><?=$row->sellerName?></p>
-                <p><?=$row->description?></p>
-                <p><?=$row->address?></p>
-                <p><?=$row->tpNumber?></p>
+
+    <div class="container center">
+        <?php foreach ($rows as $row) : ?>
+            <div class="img-con col-25">
+                <h2>Product</h2>
+                <h3><?= $row->productName ?></h3>
+                <img src="<?= ASSETS2 . $row->image ?> ">
             </div>
+            <div class="details-con col-75">
+                <div>
+                    <h2>Product Details</h2>
+                    <h4>Price : <?= "RS." . $row->productPrice ?></h4>
+                    <p><?= $row->sellerName ?></p>
+                    <p><?= $row->description ?></p>
+                    <p><?= $row->address ?></p>
+                    <p><?= $row->tpNumber ?></p>
+                </div>
 
-        </div>
-        <?php endforeach;?>
-
-        
+            </div>
+        <?php endforeach; ?>
     </div>
 
-    <script type="text/javascript" src="<?=ASSETS?>js/sellerJs"></script>
-    
-    </body>
+    <script type="text/javascript" src="<?= ASSETS ?>js/sellerJs"></script>
+
+</body>
+
 </html>
