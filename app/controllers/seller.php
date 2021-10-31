b528debffd86b4e11579b338a6b64ebaca43bb69
PS1252 <piyumsankalpaps@gmail.com>
2021-10-31 12:24:10 +0530

    updated the seller registration

--- app/controllers/seller.php
+++ app/controllers/seller.php
@@ -3,9 +3,8 @@ class seller extends Controller
 {
     function index()
     {
-        if(!Auth::logged_in())
-        {
-         $this->redirect('login/login');
+        if (!Auth::logged_in()) {
+            $this->redirect('login/login');
         }
 
         $Auth = new Auth;
@@ -15,123 +14,114 @@ class seller extends Controller
 
         $products = new products();
         $userid = Auth::userid();
-        $data = $products->where('sellerid',$userid);
-        $sellers = new sellers(); 
-        $data3 = $sellers->where('userid',$userid);
+        $data = $products->where('sellerid', $userid);
+        $sellers = new sellers();
+        $data3 = $sellers->where('userid', $userid);
 
-        if($z1=$products->where2('sellerid',$userid,'category',$type))
-        {
+        if ($z1 = $products->where2('sellerid', $userid, 'category', $type)) {
             $data4 = count($z1);
-        }
-        else{
+        } else {
             $data4 = 0;
         }
 
-        if($z2 = $products->where2('sellerid',$userid,'category',$type2))
-        {
+        if ($z2 = $products->where2('sellerid', $userid, 'category', $type2)) {
             $data5 = count($z2);
-        }
-        else{
+        } else {
             $data5 = 0;
         }
 
-        if($z3=$products->where('sellerid',$userid))
-        {
+        if ($z3 = $products->where('sellerid', $userid)) {
             $data6 = count($z3);
-        }
-        else{
+        } else {
             $data6 = 0;
         }
         //print_r($data5);
         //die;
 
 
-        $this->view('seller/seller',[
-            'rows'=>$data,
-            'data2'=>$data2,
-            'data3'=>$data3,
-            'data4'=>$data4,
-            'data5'=>$data5,
-            'data6'=>$data6,
+        $this->view('seller/seller', [
+            'rows' => $data,
+            'data2' => $data2,
+            'data3' => $data3,
+            'data4' => $data4,
+            'data5' => $data5,
+            'data6' => $data6,
         ]);
-       
     }
 
     //get the file destination
     function get_destination($destination)
     {
         global $des;
-        $des =$destination;
+        $des = $destination;
         return $des;
     }
 
     //function for registration
     function registration()
     {
-        if(!Auth::logged_in())
-        {
-         $this->redirect('login/login');
+        if (!Auth::logged_in()) {
+            $this->redirect('login/login');
+        }
+
+        $commonUser = new common_user();
+        $userid = Auth::userid();
+        $data4 = $commonUser->where('userid', $userid);
+        if ($data4) {
+            $data4 = $data4[0];
         }
 
         $errors = array();
-        if(count($_POST)>0)
-        {
-            
-            $sellers = new sellers();//create the instance of the seller in model
-            
-            
-            if($sellers->validate($_POST,$_FILES))
-            {
+        if (count($_POST) > 0) {
+
+            $sellers = new sellers(); //create the instance of the seller in model
+
+
+            if ($sellers->validate($_POST, $_FILES)) {
                 global $des;
                 $arr['userid'] = Auth::userid();
-                $arr['nameWithInitials'] = htmlspecialchars($_POST['nameWithInitials']);
+                $arr['nameWithInitials'] = $data4->nameWithInitials;
                 $arr['registrationNumber'] = htmlspecialchars($_POST['registrationNumber']);
-                $arr['tpNumber'] = htmlspecialchars($_POST['tpNumber']);
+                $arr['tpNumber'] = $data4->tpNumber;
                 //$arr['tpNumber2'] = Auth::tpNumber();
                 $arr['nic'] = htmlspecialchars($_POST['nic']);
                 $arr['address'] = htmlspecialchars($_POST['address']);
                 $arr['image'] = $des;
-             
-               
+
+
                 $sellers->insert($arr);
                 $this->redirect('home/home');
-            }
-            else{
+            } else {
                 $errors = $sellers->errors;
             }
-        } 
-        $this->view('seller/sellerregi',[
-			'errors'=>$errors,
-		]);
+        }
+        $this->view('seller/sellerregi', [
+            'errors' => $errors,
+        ]);
     }
 
     function uploadProduct()
     {
-        if(!Auth::logged_in())
-        {
-         $this->redirect('login/login');
+        if (!Auth::logged_in()) {
+            $this->redirect('login/login');
         }
 
         $errors = array();
-        
-        if(count($_POST)>0)
-        {
+
+        if (count($_POST) > 0) {
             $products = new products();
             $seller = new sellers();
             $userid = Auth::userid();
-            if(!empty($row = $seller->where('userid',$userid) ))
-            {
-               $row = $row[0];
-               $nameWithInitials= $row->nameWithInitials;
-               $address = $row->address;
-            }
-            else{
+            if (!empty($row = $seller->where('userid', $userid))) {
+                $row = $row[0];
+                $nameWithInitials = $row->nameWithInitials;
+                $address = $row->address;
+            } else {
                 $nameWithInitials = "";
                 $address = "";
             }
-          
-            if($products->validate($_POST,$_FILES))
-            {
+
+            if ($products->validate($_POST, $_FILES)) {
                 global $des;
                 $arr['productName'] = htmlspecialchars($_POST['productName']);
                 $arr['productPrice'] = htmlspecialchars($_POST['productPrice']);
@@ -142,35 +132,30 @@ class seller extends Controller
                 $arr['address'] = $address;
                 $arr['tpNumber'] = Auth::tpNumber();
                 $arr['sellerid'] = Auth::userid();
-                
+
                 $products->insert($arr);
                 $this->redirect('seller/seller');
-            }
-            else{
+            } else {
                 $errors = $products->errors2;
             }
-        } 
-        $this->view('seller/uploadProduct',[
-			'errors'=>$errors,
-		]);
-
+        }
+        $this->view('seller/uploadProduct', [
+            'errors' => $errors,
+        ]);
     }
 
     function editProduct($productid = null)
     {
-        if(!Auth::logged_in())
-        {
-         $this->redirect('login/login');
+        if (!Auth::logged_in()) {
+            $this->redirect('login/login');
         }
 
         $errors = array();
         $products = new products();
-        
-        if(count($_POST)>0)
-        {
-            
-            if($products->validate($_POST,$_FILES))
-            {
+
+        if (count($_POST) > 0) {
+
+            if ($products->validate($_POST, $_FILES)) {
                 global $des;
                 $arr['productName'] = $_POST['productName'];
                 $arr['productPrice'] = $_POST['productPrice'];
@@ -179,71 +164,59 @@ class seller extends Controller
                 $arr['category'] = $_POST['category'];
 
 
-                $products->update($productid,$arr);
+                $products->update($productid, $arr);
                 $this->redirect('seller');
-            }
-            else{
+            } else {
                 $errors = $products->errors2;
             }
         }
-        $row =$products->where('productid',$productid); //in here row is an array
-        
-        if($row)
-        {
+        $row = $products->where('productid', $productid); //in here row is an array
+
+        if ($row) {
             $row = $row[0];
-            if(file_exists($row->image)){
+            if (file_exists($row->image)) {
                 unlink($row->image);
             }
-            
         }
-        $this->view('seller/editProduct',[
-			'errors'=>$errors,
-            'row'=>$row,
-		]);
-
+        $this->view('seller/editProduct', [
+            'errors' => $errors,
+            'row' => $row,
+        ]);
     }
 
     function deleteProduct($productId = null)
     {
-        if(!Auth::logged_in())
-        {
-         $this->redirect('login/login');
+        if (!Auth::logged_in()) {
+            $this->redirect('login/login');
         }
 
         $errors = array();
         $products = new products();
-        
-        if(count($_POST)>0)
-        {
 
-                $products->delete($productId);
-                $this->redirect('seller');
-            
-            
+        if (count($_POST) > 0) {
+
+            $products->delete($productId);
+            $this->redirect('seller');
         }
-        $row =$products->where('productid',$productId); //in here row is an array
-        $data =$products->where('productid',$productId);
-        if($row)
-        {
+        $row = $products->where('productid', $productId); //in here row is an array
+        $data = $products->where('productid', $productId);
+        if ($row) {
             $row = $row[0];
             unlink($row->image);
         }
-        $this->view('seller/deleteProduct',[
-            'row'=>$row,
-            'rows'=>$data,
-		]);
-
+        $this->view('seller/deleteProduct', [
+            'row' => $row,
+            'rows' => $data,
+        ]);
     }
 
     function productDetails($productId = null)
     {
 
         $products = new products();
-        $data =$products->where('productId',$productId); 
+        $data = $products->where('productId', $productId);
 
 
-        $this->view('seller/ProductDetails',['rows'=>$data]);
-       
+        $this->view('seller/ProductDetails', ['rows' => $data]);
     }
-
 }
--- app/views/seller/sellerregi.php
+++ app/views/seller/sellerregi.php
@@ -1,89 +1,73 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Ceylon Nurture|Seller|Registration</title>
-        <?php $this -> view ("header",$data)?>
-        <link rel="stylesheet" href="<?=ASSETS?>css/registrationform.css">
 
-    </head>
+<head>
+    <title>Ceylon Nurture|Seller|Registration</title>
+    <?php $this->view("header", $data) ?>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/registrationform.css">
 
-    <body id="body">
+</head>
 
-    
+<body id="body">
+
+
     <div class="container center">
         <h1 class="h1">Update as a Seller</h1>
 
         <!--for the errors-->
-        <?php if(count($errors) > 0 ):?>
-        <div class="alertwarning">
-        <button class="closebtn" onclick="closebutton()">&times;</button>   
-            <strong>Error!</strong> 
-            <?php foreach($errors as $error ):?>
-                <br /><?=$error?>
-            <?php endforeach;?>
-        </div>
-        <?php endif;?>
+        <?php if (count($errors) > 0) : ?>
+            <div class="alertwarning">
+                <button class="closebtn" onclick="closebutton()">&times;</button>
+                <strong>Error!</strong>
+                <?php foreach ($errors as $error) : ?>
+                    <br /><?= $error ?>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
 
         <hr>
         <form class="regi_form" enctype="multipart/form-data" method="POST">
 
+            
             <div class="row">
-            <div class="col-25">
-                <label for="nameWithInitials">Name With Initials</label>
-            </div>
-            <div class="col-75">
-                <input type="text" value="<?=get_var('nameWithInitials')?>" id="nameWithInitials" name="nameWithInitials" placeholder="Name With Initials">
-            </div>
+                <div class="col-25">
+                    <label for="registrationNumber">Registration Number</label>
+                </div>
+                <div class="col-75">
+                    <input type="text" value="<?= get_var('registrationNumber') ?>" id="registrationNumber" name="registrationNumber" placeholder="Registration Number">
+                </div>
             </div>
 
             <div class="row">
-            <div class="col-25">
-                <label for="registrationNumber">Registration Number</label>
-            </div>
-            <div class="col-75">
-                <input type="text" value="<?=get_var('registrationNumber')?>" id="registrationNumber" name="registrationNumber" placeholder="Registration Number">
-            </div>
-            </div>
-
-            <div class="row">
-            <div class="col-25">
-                <label for="tpNumber">TP Number</label>
-            </div>
-            <div class="col-75">
-            <input type="text" value="<?=get_var('tpNumber')?>" id="tpNumber" name="tpNumber" placeholder="TP Number">
-            </div>
+                <div class="col-25">
+                    <label for="nic">NIC</label>
+                </div>
+                <div class="col-75">
+                    <input type="text" value="<?= get_var('nic') ?>" id="nic" name="nic" placeholder="NIC">
+                </div>
             </div>
 
             <div class="row">
-            <div class="col-25">
-                <label for="nic">NIC</label>
-            </div>
-            <div class="col-75">
-            <input type="text" value="<?=get_var('nic')?>" id="nic" name="nic" placeholder="NIC">
-            </div>
-            </div>
-
-            <div class="row">
-            <div class="col-25">
-                <label for="address">Address</label>
-            </div>
-            <div class="col-75">
-            <input type="text" value="<?=get_var('address')?>" id="address" name="address" placeholder="Address">
-            </div>
+                <div class="col-25">
+                    <label for="address">Address</label>
+                </div>
+                <div class="col-75">
+                    <input type="text" value="<?= get_var('address') ?>" id="address" name="address" placeholder="Address">
+                </div>
             </div>
 
             <div class="row">
-            <div class="col-25">
-                <label for="image" >Image of the Certificate</label>
-            </div>
-            <div class="col-75">
-            <input type="file" id="image" value="<?=get_var('image')?>" name="image">
-            </div>
+                <div class="col-25">
+                    <label for="image">Image of the Certificate</label>
+                </div>
+                <div class="col-75">
+                    <input type="file" id="image" value="<?= get_var('image') ?>" name="image">
+                </div>
             </div>
 
             <div class="row">
-            <input type="submit" value="Submit">
-            <input type="reset" value="Reset">
+                <input type="submit" value="Submit">
+                <input type="reset" value="Reset">
             </div>
         </form>
     </div>
@@ -91,7 +75,8 @@
     <!--footer-->
     <?php $this->view("footer") ?>
     <!--end of footer-->
-    <script type="text/javascript" src="<?=ASSETS?>js/sellerJs"></script>
-    
-    </body>
+    <script type="text/javascript" src="<?= ASSETS ?>js/sellerJs"></script>
+
+</body>
+
 </html>
