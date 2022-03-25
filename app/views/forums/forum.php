5014d505ef5ea1ac66c447c5462d3b7d7207ca16
cham dil <cham.dil333@outlook.com>
2022-03-25 23:57:22 +0530

    adding updates to forums

--- app/views/forums/forum.php
+++ app/views/forums/forum.php
@@ -91,7 +91,7 @@
                         <div class=container>
 
                             <div  class="body_content">
-                                <div> <a href="<?=ROOT?>forum/addForumDoctor"><button class="add">Start a new discussion<i class="fas fa-long-arrow-alt-right"></i></button></a></div><br>
+                                <div> <a href="<?=ROOT?>forum/addForumHerb"><button class="add">Start a new discussion<i class="fas fa-long-arrow-alt-right"></i></button></a></div><br>
                                 <div> <button class="add A"> <i class="far fa-comment-alt"></i>All discussions</button></a></div>
                                 <div> <button class="add A"><i class="fas fa-forward"></i> Newest discussions</button></a></div>
                                 <div> <button class="add A"><i class="fas fa-certificate"></i> Verified discussions</button></a></div>
@@ -102,43 +102,49 @@
                             </div>
 
                             <div class="body_container_main_sub">
-                                        <div class='body_container'>
+                                <?php if ($rows1) : ?>
+                                    <?php foreach ($rows1 as $row):?>
+                                    <div class='body_container'>
+                                        
+                                        <div class='body_container1'>
+                                            <div class="user">
+                                                <div class="user_image"><img class="profile_pic" src="<?=ASSETS?>img/profile_picture1.jpg"/></div>
+                                                <!--<img src="https://img.icons8.com/fluency/48/000000/edit-user-male.png"/>-->
+                                                <div class="user_name">Name</div>
+                                            </div>
+                                        </div>
+                                        <div class = 'body_container2'>
+                                            <div class="line1">
+                                                <h3> <?=esc($row->name)?> </h3>
+                                                <div class="available"> 
+                                                <a href="<?=ROOT?>channeling/doctors/2?"><button class="forum_button view">View Article</button> </a>
+                                                </div>
+                                            </div>
                                             
-                                            <div class='body_container1'>
-                                                <div class="user">
-                                                    <div class="user_image"><img class="profile_pic" src="<?=ASSETS?>img/profile_picture1.jpg"/></div>
-                                                    <!--<img src="https://img.icons8.com/fluency/48/000000/edit-user-male.png"/>-->
-                                                    <div class="user_name">Peter</div>
-                                                </div>
+                                            <br>
+                                            <div class="line2"><p style="text-align:left;"><?=esc($row->description)?></p> </div>
+                                            <div class="image"><img class="image_forum" src="<?= ASSETS2 . $row->image ?> "></div><br> 
+                                            
+                                            <div class="forum_buttons">
+
+                                            <?php if ($rows3) : ?>
+                                            <a href="<?=ROOT?>doctor/addArticles/<?=$rows3[0]->userid?>"><button class="forum_button update">Verify</button></a>
+                                            <?php endif; ?> 
+
+                                            <a href="<?=ROOT?>forum/updateForumHerb/<?=$row->forumHerbid?>"><button class="forum_button update">Update</button></a>
+                                            <a href="<?=ROOT?>forum/deleteForumHerb/<?=$row->forumHerbid?>"><button class="forum_button delete">Delete</button></a>
+                                            <button class="forum_button">reply</button>
                                             </div>
-                                            <div class = 'body_container2'>
-                                                <div class="line1">
-                                                    <h3> Ashwagandha </h3>
-                                                    <div class="available"> 
-                                                    <a href="<?=ROOT?>articles/articleDetails/2"><button class="forum_button view">View Article</button> </a>
-                                                    </div>
-                                                </div>
-                                                
-                                                <br>
-                                                <div class="line2"><p style="text-align:left;">Ashwagandha is a very good herb for stress. you have to take it twice a day. i have those herbs in my garden. please contact me if you want it.</p> </div><br> 
-                                                <div class="line3"><p style="text-align:left;">Contact Number: 0777986455 </p></div>
-                                                <div class="image"><img class="forum_image" src="<?=ASSETS?>img/forum1.jpeg"/></div><br>
-
-                                                <div class="line3"><p style="text-align:left; color:green;">Verified Content <br>Checked by:Dr.Thush Perera<br>General Physician</p></div>
-
-
-
-                                                <div class="forum_buttons">
-                                                <a href="<?=ROOT?>forum/updateForumDoctor/<?=$row->forumDoctorid?>"><button class="forum_button update">Update</button></a>
-                                                <a href="<?=ROOT?>forum/deleteForumDoctor/<?=$row->forumDoctorid?>"><button class="forum_button delete">Delete</button></a>
-                                                <button class="forum_button">reply</button>
-                                                </div>
-                                            </div>
-
                                         </div>
 
+                                    </div>
 
-                            </div>
+                                    <?php endforeach;?>
+                                <?php else : ?>
+                                <h4>No Forums to show</h4>
+                                <?php endif; ?> 
+
+                        </div>
 
 
 
@@ -154,7 +160,7 @@
                     <div class=container>
 
                             <div  class="body_content">
-                                <div> <a href="<?=ROOT?>forum/addForumDoctor"><button class="add">Start a new discussion<i class="fas fa-long-arrow-alt-right"></i></button></a></div><br>
+                                <div> <a href="<?=ROOT?>forum/addForumProduct"><button class="add">Start a new discussion<i class="fas fa-long-arrow-alt-right"></i></button></a></div><br>
                                 <div> <button class="add A"> <i class="far fa-comment-alt"></i>All discussions</button></a></div>
                                 <div> <button class="add A"><i class="fas fa-forward"></i> Newest discussions</button></a></div>
                                 <div> <button class="add A"><i class="fas fa-certificate"></i> Verified discussions</button></a></div>
@@ -165,41 +171,43 @@
                             </div>
 
                             <div class="body_container_main_sub">
-                                        <div class='body_container'>
+                                <?php if ($rows2) : ?>
+                                    <?php foreach ($rows2 as $row):?>
+                                    <div class='body_container'>
+                                        
+                                        <div class='body_container1'>
+                                            <div class="user">
+                                                <div class="user_image"><img class="profile_pic" src="<?=ASSETS?>img/profile_picture1.jpg"/></div>
+                                                <!--<img src="https://img.icons8.com/fluency/48/000000/edit-user-male.png"/>-->
+                                                <div class="user_name">Name</div>
+                                            </div>
+                                        </div>
+                                        <div class = 'body_container2'>
+                                            <div class="line1">
+                                                <h3> <?=esc($row->name)?> </h3>
+                                                <div class="available"> 
+                                                <a href="<?=ROOT?>channeling/doctors/2?"><button class="forum_button view">View Product</button> </a>
+                                                </div>
+                                            </div>
                                             
-                                            <div class='body_container1'>
-                                                <div class="user">
-                                                    <div class="user_image"><img class="profile_pic" src="<?=ASSETS?>img/profile_picture1.jpg"/></div>
-                                                    <!--<img src="https://img.icons8.com/fluency/48/000000/edit-user-male.png"/>-->
-                                                    <div class="user_name">Peter</div>
-                                                </div>
+                                            <br>
+                                            <div class="line2"><p style="text-align:left;"><?=esc($row->description)?></p> </div>
+                                            <div class="image"><img class="image_forum" src="<?= ASSETS2 . $row->image ?> "></div><br> 
+                                            <div class="forum_buttons">
+                                            <a href="<?=ROOT?>forum/updateForumDoctor/<?=$row->forumProductid?>"><button class="forum_button update">Update</button></a>
+                                            <a href="<?=ROOT?>forum/deleteForumDoctor/<?=$row->forumProductid?>"><button class="forum_button delete">Delete</button></a>
+                                            <button class="forum_button">reply</button>
                                             </div>
-                                            <div class = 'body_container2'>
-                                                <div class="line1">
-                                                    <h3> Ashwagandha Root Powder </h3>
-                                                    <div class="available">
-                                                    <a href="<?=ROOT?>seller/productDetails/1"><button class="forum_button view">View Product</button></a>  
-                                                    </div>
-                                                </div>
-                                                
-                                                <br>
-                                                <div class="line2"><p style="text-align:left;">Spring Valley Ashwagandha Root Powder Vegetarian Capsules are dietary supplements that may promote general wellness. </p> </div><br> 
-                                                <div style="width:250px;" class="image"><img class="forum_image" src="<?=ASSETS?>img/forum2.jpeg"/></div><br>
-
-
-
-
-                                                <div class="forum_buttons">
-                                                <a href="<?=ROOT?>forum/updateForumDoctor/<?=$row->forumDoctorid?>"><button class="forum_button update">Update</button></a>
-                                                <a href="<?=ROOT?>forum/deleteForumDoctor/<?=$row->forumDoctorid?>"><button class="forum_button delete">Delete</button></a>
-                                                <button class="forum_button">reply</button>
-                                                </div>
-                                            </div>
-
                                         </div>
 
+                                    </div>
 
-                            </div>
+                                    <?php endforeach;?>
+                                <?php else : ?>
+                                <h4>No Forums to show</h4>
+                                <?php endif; ?> 
+
+                        </div>
 
 
 
